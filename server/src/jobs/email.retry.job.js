7d669abcf0adb6c1c404ddc8e5a90c7b408cb57f
NotArsal <arsalanshaikh5123@gmail.com>
2026-06-30 18:45:54 +0530

    feat: change email queue retry interval from 10m to 1m

--- server/src/jobs/email.retry.job.js
+++ server/src/jobs/email.retry.job.js
@@ -4,6 +4,6 @@ export const initEmailRetryJob = () => {
   // Run immediately on startup (optional, but good if we just booted)
   processEmailQueue();
 
-  // Run every 10 minutes
-  setInterval(processEmailQueue, 10 * 60 * 1000);
+  // Run every 1 minute
+  setInterval(processEmailQueue, 60 * 1000);
 };
